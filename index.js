ace6fd980187deda79386cb31a0afe8c6fa49367
Taffie Theuri <taffietheuri4@gmail.com>
2022-12-14 11:03:31 +0300

    Completed

--- index.js
+++ index.js
@@ -1,25 +1,51 @@
-function isPalindrome(word) {
-  // Write your algorithm here
-}
 
-/* 
-  Add your pseudocode here
-*/
+  function isPalindrome(word) {
+    // Write your algorithm here
+    return word.split("").reverse().join("");
 
-/*
-  Add written explanation of your solution here
-*/
 
-// You can run `node index.js` to view these console logs
-if (require.main === module) {
-  // add your own custom tests in here
-  console.log("Expecting: true");
-  console.log("=>", isPalindrome("racecar"));
 
-  console.log("");
+  }
+  function reverse(word) {
+    // 'abc' => 'cba'
+    // const wordArray = word.split("");
+    // const reversedWordArray = wordArray.reverse();
+    // const reversedWord = reversedWordArray.join("");
+    // return reversedWord;
+    return word.split("").reverse().join("");
+  }
 
-  console.log("Expecting: false");
-  console.log("=>", isPalindrome("robot"));
-}
+  function isPalindrome(word) {
+    const reversedWord = reverse(word);
 
-module.exports = isPalindrome;
+    return word === reversedWord;
+  }
+  /*
+    Add your pseudocode here
+  */
+  //return word.split("").reverse().join("");
+
+  /*
+    Add written explanation of your solution here
+  */
+  // 'Taffie ' will reverse to 'eiffaT'
+  // const word_array = word.split(""); // using .split to remove blank spaces /spliting a string into an array of substrings
+  // const reverse = wordArray.reverse(); // using .reverse to reverse the word
+  // const reversed_word = reversedWordArray.join("");  //returning our array as a string using the join method
+  // return reversed_word;
+
+  // You can run `node index.js` to view these console logs
+  if (require.main === module) {
+    // add your own custom tests in here
+    console.log("Expecting: true");
+    console.log("=>", isPalindrome("racecar"));
+
+    console.log("");
+
+    console.log("Expecting: false");
+    console.log("=>", isPalindrome("robot"));
+  }
+
+  module.exports = isPalindrome;
+
+
